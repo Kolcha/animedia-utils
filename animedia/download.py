23546aeab9f163cf9511f9634de4969918ddff33
Nick Korotysh <kolchaprogrammer@list.ru>
2020-03-15 11:30:58 +0300

    version 0.1.0

---
+++ animedia/download.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import cgi
+import requests
+
+from lxml import html
+
+
+def _get_torrent_links(content):
+    tree = html.fromstring(content)
+    nodes = tree.xpath('//div[@class="download_tracker"]/a')
+    return [n.get('href') for n in nodes if n.get('href').startswith('http')]
+
+
+def _get_torrent_file_name(headers):
+    value, params = cgi.parse_header(headers['Content-Disposition'])
+    return params['filename']
+
+
+def _download_torrent_file(link, session):
+    r = session.get(link)
+    filename = _get_torrent_file_name(r.headers)
+    return (filename, r.content)
+
+
+def download_torrents(link, session=None):
+    if not session:
+        session = requests.Session()
+    r = session.get(link)
+    if r.status_code != 200:
+        return []
+    torrent_links = _get_torrent_links(r.content)
+    return [_download_torrent_file(t, session) for t in torrent_links]
+
+
+if __name__ == '__main__':
+    ts = download_torrents('https://tt.animedia.tv/anime/vrata-shtajnera')
+    for t in ts:
+        with open(t[0], "wb") as f:
+            f.write(t[1])
---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+setuptools.setup(
+    name="animedia-utils",
+    version="0.1.0",
+    author="Nick Korotysh",
+    author_email="kolchaprogrammer@list.ru",
+    packages=setuptools.find_packages(),
+    install_requires=['lxml', 'requests'],
+    python_requires='>=3',
+)
