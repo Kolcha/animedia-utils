ebb82eeccd6da40eb97981c027b4af43c83f6f5a
Nick Korotysh <kolchaprogrammer@list.ru>
2021-04-17 11:30:59 +0300

    version 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="animedia-utils",
-    version="0.1.2",
+    version="0.1.3",
     author="Nick Korotysh",
     author_email="kolchaprogrammer@list.ru",
     description="download torrents from tt.animedia.tv",
